1b9fd637310ce0d5c0a4f73d384c148fdd7c0f02
dancouver <danhathway@gmail.com>
2017-03-02 14:08:23 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()) {
+## This creates a matrix as described by x in Cache and provides 4 functions needed to manipulate it
+  
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  invset <- function (inverse) inv <<- inverse  
+  invget <- function() inv
+  list(set=set, get=get, invset=invset, invget=invget)
+  
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+## This returns a matrix that is the inverse of 'x'
+  
+  inv_m = x$invget()
+  if(!is.null(inv_m)){
+    message("retrieving from cache")
+    return(inv_m)
+    }
+   else {
+     m_data = x$get()
+     if ( det(m_data) == 0 ) {
+       message("non invertible matrix")
+       ## I put this in here, because I spent a lot of time trying to figure why my initial test matrix failed
+       ## It turned out to be "singular / non invertible" [1:9] 3 rows, 3 columns is an example!
+       }
+     else {
+       inv_m=solve(m_data,...)
+       x$invset(inv_m)
+       return(inv_m)
+       }
+     }
 }
